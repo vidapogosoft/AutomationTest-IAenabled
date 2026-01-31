69f32917920352bec7088dc7c45cdd0f1e300d91
vidapogosoft <vidapogosoft@hotmail.com>
2026-01-31 11:03:18 -0500

    Taller subida inicial dia 3

---
+++ ApiTaller1/Interface/IOrdenRecibo.cs
@@ -0,0 +1,22 @@
+﻿using ApiTaller1.Model.dto;
+using ormdb.Models.Database;
+
+namespace ApiTaller1.Interface
+{
+    public interface IOrdenRecibo
+    {
+        //se define los metodos que se van a implementar en la clase OrdenRecibo
+
+        IEnumerable<OrdenReciboRevisada> ConsultaOrdenesRevisadas { get; }
+
+        IEnumerable<OrdenReciboRevisada> ConsultaOrdenRevisada(string ruc);
+
+        OrdenReciboRevisada? ConsultaOrdenRevisadaObject(string numorden);
+
+        IEnumerable<DtoORProductosRevisados> ConsultaProductosRevisados { get; }
+        IEnumerable<DtoORProductosRevisados> ConsultaProductosRevisadosv2(string ordenrecibo, int idorden);
+
+        IEnumerable<ormdb.Models.DTO.DtoORProductosRevisados> ConsultaProductosRevisadosSP { get; }
+
+    }
+}
